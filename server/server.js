d4d4b42092d0fef00f996ac99aad2f8ed7c589a7
Diana Moretti <morettidn@gmail.com>
2025-06-02 09:50:49 +0200

    build configuration

--- server/server.js
+++ server/server.js
@@ -2,17 +2,29 @@ import express from "express";
 import dotenv from "dotenv";
 import dreamRoutes from "./routes/dream.routes.js";
 import { connectDb } from "./config/db.js";
+import path from 'path'
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+
 app.use(express.json());
 
 app.use('/api/dreams', dreamRoutes);
 
 
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  })
+}
+
 app.listen(PORT, () => {
   connectDb();
-  console.log("Server is running at http://localhost:" + PORT);
+  // console.log("Server is running at http://localhost:" + PORT);
 });
