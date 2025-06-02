d8c4f27bf27c60e821df5ba280b86a4663e490dc
Diana Moretti <morettidn@gmail.com>
2025-06-02 18:02:31 +0200

    update server

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use('/api/dreams', dreamRoutes);
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/dist")));
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   })
 }
