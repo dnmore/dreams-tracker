a6bd1cbd7bf5049bf14f6eb106bd5ef151bbaf0e
Diana Moretti <morettidn@gmail.com>
2025-06-02 17:30:38 +0200

    update server

--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import dreamRoutes from "./routes/dream.routes.js";
 import { connectDb } from "./config/db.js";
-import path from 'path'
+import path, { dirname} from 'path';
+import { fileURLToPath } from "url";
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename);
 
 
 app.use(express.json());
-
 app.use('/api/dreams', dreamRoutes);
 
 
